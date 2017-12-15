c7f70fd7fa28d39525f9129ffc77e4cfd01efcd4
andy-shi88 <shi88.andy@gmail.com>
2017-12-15 12:13:37 +0700

    add argument parser and clean up

--- run.py
+++ run.py
@@ -1,14 +1,45 @@
+import argparse, asyncio, sys # noqa
 from bot import Bot # noqa
+import constants
 
-BOT_TOKEN = "<your token>"
 
-
-def main():
+def main(args):
 	"""Execute bot."""
-	lady = Bot(BOT_TOKEN)
-	lady = lady.set_question(['A', 'B', 'C']).set_channels(
-		['batch1', 'batch2'])
+	batches = args.batches.split(',')
+	args.token = constants.BOT_TOKEN
+	if args.token is None:
+		print('''
+token is required, provide them by --token=xxx,
+or run --help for further information.
+		''')
+		return
+	lady = Bot(args.token)
+	lady = lady.set_questions(constants.QUESTIONS).set_channels(
+		batches)
 	lady.start()
+	lady.send_report(args.hook)
+
+
+def parse_arguments():
+	"""Parse arguments."""
+	parser = argparse.ArgumentParser(
+		description=constants.BASE_DESCRIPTION,
+		epilog=constants.CREATOR)
+
+	parser.add_argument('--batches', type=str, default=constants.DEFAULT_CHANNEL, help='''
+		Put in channel name separated by comma(,)
+		without spaces.
+		''')
+
+	parser.add_argument('--hook', type=str, default=constants.DEFAULT_HOOK, help='''
+		Hook endpoint for the standup report to send to.
+		''')
+
+	parser.add_argument('--token', type=str, default=None, help='''
+		Your bot token.
+		''')
+	return parser.parse_args()
 
 if __name__ == '__main__':
-	main()
+	args = parse_arguments()
+	main(args)
